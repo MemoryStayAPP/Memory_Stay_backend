3a63f5041dfab6d5d7b55d26e11f8a08edb413f1
Sirdolorem <87831311+Sirdolorem@users.noreply.github.com>
2022-11-09 15:00:32 +0100

    laravel template

---
+++ MemoryStay_backend/app/Http/Controllers/Api/MarkerController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MarkerController extends Controller
+{
+    public function createMarker(Request $request){
+
+    }
+    public funcion
+}
