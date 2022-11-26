e06175e0412f314b9590e05096ad2e78f4bf47da
Sirdolorem <87831311+Sirdolorem@users.noreply.github.com>
2022-11-26 18:22:42 +0100

    naprawione tokeny

--- Memory_Stay/app/Http/Kernel.php
+++ Memory_Stay/app/Http/Kernel.php
@@ -40,10 +40,6 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-//            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-//            'throttle:api',
-//            \Fruitcake\Cors\HandleCors::class,
-//            \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
 
