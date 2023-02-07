1e8eaaf2b521337504f6ceb96af26cc733d4f33a
Igor Żurawski <87831311+Sirdolorem@users.noreply.github.com>
2023-02-07 13:43:35 +0100

    Update api.php

--- Memory_Stay/routes/api.php
+++ Memory_Stay/routes/api.php
@@ -23,10 +23,10 @@ use App\Http\Controllers\Auth\EmailVerificationController;
 
 Route::post('/auth/register', [CreateUserController::class, 'createUser']);
 Route::post('/auth/login', [LoginUserController::class, 'loginUser']);
-Route::post('/auth/delete', [DeleteUserController::class, 'deleteUser']);
-Route::post('/markers/create', [CreateController::class, 'createMarker']);
-Route::post('/markers/delete', [DeleteController::class, 'deleteMarker']);
-Route::post('/markers/select', [SelectController::class, 'selectMarker']);
+Route::post('/auth/delete', [DeleteUserController::class, 'deleteUser'])->middleware('auth:sanctum');
+Route::post('/markers/create', [CreateController::class, 'createMarker'])->middleware('auth:sanctum');
+Route::post('/markers/delete', [DeleteController::class, 'deleteMarker'])->middleware('auth:sanctum');
+Route::post('/markers/select', [SelectController::class, 'selectMarker'])   ;
 Route::get('/markers/get', [SelectController::class, 'getMarkers']);
 
 Route::post('/auth/email/verification-notification', [EmailVerificationController::class, 'sendVerificationEmail'])->middleware('auth:sanctum');
