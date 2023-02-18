0b660928f6397da25f5cc32376a62683ad86c95f
Igor Żurawski <87831311+Sirdolorem@users.noreply.github.com>
2023-02-18 15:27:07 +0100

    naprawione sprawdzanie hasel i logowanie sie przez maila

--- Memory_Stay/app/Http/Requests/UserCreateRequest.php
+++ Memory_Stay/app/Http/Requests/UserCreateRequest.php
@@ -1,35 +1,83 @@
 <?php
 
-namespace App\Http\Requests;
+// namespace App\Http\Requests;
 
+// use Illuminate\Foundation\Http\FormRequest;
+
+
+
+
+
+
+// class UserCreateRequest extends FormRequest
+
+// {
+    
+    //     public function rules()
+    
+    //     {
+        
+        //         return [
+            
+            //             'name' => 'required',
+            //             'email' => 'required|email:rfc,dns|unique:users,email',
+            //             'password' => 'required'
+            
+            //         ];
+            
+            //     }
+            
+            
+            
+            //     public function failedValidation(Validator $validator)
+            
+            //     {
+                
+                //         throw new HttpResponseException(response()->json([
+                    
+                    //             'success'   => false,
+                    
+                    //             'message'   => 'Validation errors',
+                    
+                    //             'data'      => $validator->errors()
+                    
+                    //         ], 401));
+                    
+                    //     }
+                    
+                    
+                    // }
+                    
+                    namespace App\Http\Requests;
+                    
 use Illuminate\Foundation\Http\FormRequest;
-
-use Illuminate\Http\Exceptions\HttpResponseException;
-
 use Illuminate\Contracts\Validation\Validator;
-
-
-
+use Illuminate\Http\Exceptions\HttpResponseException;   
 class UserCreateRequest extends FormRequest
-
 {
-
-    public function rules()
-
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
     {
-
-        return [
-
-            'name' => 'required',
-            'email' => 'required|email:rfc,dns|unique:users,email',
-            'password' => 'required'
-
-        ];
-
+        return true;
     }
 
-
-
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'email' => 'required|email:rfc,dns|unique:users,email',
+            'username' => 'required|unique:users,username',
+            'password' => 'required|min:8', 
+        ];
+    }
     public function failedValidation(Validator $validator)
 
     {
@@ -45,6 +93,4 @@ class UserCreateRequest extends FormRequest
         ], 401));
 
     }
-
-
 }
--- Memory_Stay/database/migrations/2014_10_12_000000_create_users_table.php
+++ Memory_Stay/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,14 +15,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->string('name');
+            // $table->string('uuid')->unique();
+            $table->string('name')->nullable();
             $table->string('email')->unique();
+            $table->string('username')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-        });
+         });
     }
 
     /**
